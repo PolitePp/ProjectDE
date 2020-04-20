87fd672962099659d35ccec2a252ca2f35d8da53
PolitePp <47497083+PolitePp@users.noreply.github.com>
2020-04-20 21:06:11 +0500

    Update etl.py

--- PythonScripts/etl.py
+++ PythonScripts/etl.py
@@ -102,70 +102,70 @@ df2 = spark.read \
 
 df2.createOrReplaceTempView("op_rendered_service")
 
-# ors_hub = spark.sql('''select cast(id as int) as id_rendered_service
-#                     , md5(cast(id as String)) as hk_rendered_service
-#                     , now() as load_date
-#                     , 1 as record_source
-#                     from op_rendered_service''')
-#
-# ors_hub.printSchema()
-#
-# ors_hub.write.format('com.vertica.spark.datasource.DefaultSource') \
-#     .option('db', 'docker') \
-#     .option('user', 'dbadmin') \
-#     .option('password', '123456') \
-#     .option('table', 'hub_rendered_service') \
-#     .option('host', '172.24.0.7') \
-#     .option('hdfs_url', 'hdfs://172.24.0.4:9000/user/hadoop') \
-#     .option('web_hdfs_url', 'webhdfs://172.24.0.4:9870/user/hadoop') \
-#     .mode('append') \
-#     .save()
-#
-# ors_sat = spark.sql('''select md5(cast(id as String)) as hk_rendered_service
-#                     , now() as load_date
-#                     , md5(concat_ws(',', sst365_guid, idtypeservice, dms_service_name, idmcunit, services_count
-#                     , idtypepayment, polis_number, polis_series, mdn366_guid, specialist_id, tariff
-#                     , service_date, service_start_date, service_end_date, policy_type, diagnoses_id
-#                     , profile_id, children_profile, id_researched_materials, hst0444_id, refusal_sign
-#                     , medical_intervention, id_hst0494, id_fed00045, id_hst0491)) as hash_diff
-#                     , 1 as record_source
-#                     , sst365_guid
-#                     , idtypeservice
-#                     , dms_service_name
-#                     , idmcunit
-#                     , services_count
-#                     , idtypepayment
-#                     , polis_number
-#                     , polis_series
-#                     , mdn366_guid
-#                     , specialist_id
-#                     , tariff
-#                     , service_date
-#                     , service_start_date
-#                     , service_end_date
-#                     , policy_type
-#                     , diagnoses_id
-#                     , profile_id
-#                     , children_profile
-#                     , id_researched_materials
-#                     , hst0444_id
-#                     , refusal_sign
-#                     , medical_intervention
-#                     , id_hst0494
-#                     , id_fed00045
-#                     , id_hst0491
-#                     from op_rendered_service''')
-#
-# ors_sat.write.format('com.vertica.spark.datasource.DefaultSource') \
-#     .option('db', 'docker') \
-#     .option('user', 'dbadmin') \
-#     .option('password', '123456') \
-#     .option('table', 'sat_rendered_service') \
-#     .option('host', '172.24.0.7') \
-#     .option('hdfs_url', 'hdfs://172.24.0.4:9000/user/hadoop') \
-#     .option('web_hdfs_url', 'webhdfs://172.24.0.4:9870/user/hadoop') \
-#     .mode('append') \
-#     .save()
+ors_hub = spark.sql('''select cast(id as int) as id_rendered_service
+                    , md5(cast(id as String)) as hk_rendered_service
+                    , now() as load_date
+                    , 1 as record_source
+                    from op_rendered_service''')
+
+ors_hub.printSchema()
+
+ors_hub.write.format('com.vertica.spark.datasource.DefaultSource') \
+    .option('db', 'docker') \
+    .option('user', 'dbadmin') \
+    .option('password', '123456') \
+    .option('table', 'hub_rendered_service') \
+    .option('host', '172.24.0.7') \
+    .option('hdfs_url', 'hdfs://172.24.0.4:9000/user/hadoop') \
+    .option('web_hdfs_url', 'webhdfs://172.24.0.4:9870/user/hadoop') \
+    .mode('append') \
+    .save()
+
+ors_sat = spark.sql('''select md5(cast(id as String)) as hk_rendered_service
+                    , now() as load_date
+                    , md5(concat_ws(',', sst365_guid, idtypeservice, dms_service_name, idmcunit, services_count
+                    , idtypepayment, polis_number, polis_series, mdn366_guid, specialist_id, tariff
+                    , service_date, service_start_date, service_end_date, policy_type, diagnoses_id
+                    , profile_id, children_profile, id_researched_materials, hst0444_id, refusal_sign
+                    , medical_intervention, id_hst0494, id_fed00045, id_hst0491)) as hash_diff
+                    , 1 as record_source
+                    , sst365_guid
+                    , idtypeservice
+                    , dms_service_name
+                    , idmcunit
+                    , services_count
+                    , idtypepayment
+                    , polis_number
+                    , polis_series
+                    , mdn366_guid
+                    , specialist_id
+                    , tariff
+                    , service_date
+                    , service_start_date
+                    , service_end_date
+                    , policy_type
+                    , diagnoses_id
+                    , profile_id
+                    , children_profile
+                    , id_researched_materials
+                    , hst0444_id
+                    , refusal_sign
+                    , medical_intervention
+                    , id_hst0494
+                    , id_fed00045
+                    , id_hst0491
+                    from op_rendered_service''')
+
+ors_sat.write.format('com.vertica.spark.datasource.DefaultSource') \
+    .option('db', 'docker') \
+    .option('user', 'dbadmin') \
+    .option('password', '123456') \
+    .option('table', 'sat_rendered_service') \
+    .option('host', '172.24.0.7') \
+    .option('hdfs_url', 'hdfs://172.24.0.4:9000/user/hadoop') \
+    .option('web_hdfs_url', 'webhdfs://172.24.0.4:9870/user/hadoop') \
+    .mode('append') \
+    .save()
 
 doc_ors_lnk = spark.sql('''select md5(concat(id, reg_doc_id)) as hk_doc_reg_rendered_service
                         , md5(cast(id as String)) as hk_rendered_service
